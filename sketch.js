a01cd96eb38c9c6f9dacbddc6d4bf91dc0f7bac4
robichy98 <63616953+robichy98@users.noreply.github.com>
2020-05-11 18:32:03 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var robi=0;
+
+
+function setup() {
+  createCanvas(400, 400);
+background("#BDD8B5");
+}
+
+function draw() {
+  
+  fill(140,robi,60);
+  stroke("#104D1D");
+  strokeWeight(5);
+  ellipse(200,200,robi,robi);
+  
+  fill("#24C36B");
+  ellipse(mouseX, mouseY, 50, 50);
+  textSize(24);
+  textFont("Georgia");
+  textStyle(ITALIC);
+  text("Good Morning !", 200, 100);
+  text("poke me", 305,305);
+  rect(200,200,100,100);
+  line(200,200,100,robi);
+
+}
+function mousePressed(){
+  
+  robi = robi+10;
+  
+}
